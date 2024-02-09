de7ff5677735dd4efd780f226738b2011988f720
Angeliki Vlachou <angeliki_vl@outlook.com>
2024-02-09 14:43:25 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
     button.addEventListener("click", function () {
       // Check if the clicked button has the "submit" data attribute
       if (this.getAttribute("data-type") === "submit") {
-        // If it does, display an alert saying "You clicked Submit!"
-        alert("You clicked Submit!");
+        checkAnswer();
       } else {
         // If it doesn't, get the value of the "data-type" attribute
         let gameType = this.getAttribute("data-type");
@@ -43,9 +42,42 @@ function runGame(gameType) {
 
 }
 
-function checkAnswer() {}
+/**
+ * Checks the answer against the first element in 
+ * the returned calculatedCorrectAnswer array
+ */
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
-function calculateCorrectAnswer() {}
+    if (isCorrect){
+        alert('Hey! You got it right! :D');
+    } else{
+        alert (`Aww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
+
+}
+
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus, etc)
+ * directly from the dom and returns the correct answer.
+ */
+function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+
+    if (operator === '+'){
+        return [operand1 + operand2, 'addition'];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+
+}
 
 function incrementScore() {}
 
